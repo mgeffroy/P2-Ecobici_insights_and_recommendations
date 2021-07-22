8a61a21186925a976405234a0b2a9056241d4144
kennethcandersen <kcab98@gmail.com>
2021-07-22 14:32:24 -0500

    About us draft added

--- Ecobici/static/js/config.js
+++ Ecobici/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-API_KEY = "pk.eyJ1Ijoia2NhYjk4IiwiYSI6ImNrcXNxa3J6MTFxcGkycHA1cG94cGllZTQifQ.wNODXbHIuogHvdKHyBe6VA"
+const API_KEY = "pk.eyJ1Ijoia2NhYjk4IiwiYSI6ImNrcXNxa3J6MTFxcGkycHA1cG94cGllZTQifQ.wNODXbHIuogHvdKHyBe6VA"
