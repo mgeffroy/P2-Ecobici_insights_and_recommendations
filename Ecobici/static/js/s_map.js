4448dd667c20ab1275af592deb99a10bf00d7697
kennethcandersen <kcab98@gmail.com>
2021-07-22 22:06:30 -0500

    Stations mpa and routes map finished. Just need to be plugged into API

--- Ecobici/static/js/s_map.js
+++ Ecobici/static/js/s_map.js
@@ -5,7 +5,7 @@ var myMap = L.map("s_map", {
 });
 
 // Adding tile layer to the map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var streetView = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
@@ -18,7 +18,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 var markers_url = "static/temp_data/stationdata.json";
 
 // Grab the data with d3
-d3.json(markers_url).then(function(response) {
+d3.json(markers_url).then(function (response) {
 
   // Create a new marker cluster group
   var markers = L.markerClusterGroup();
@@ -34,49 +34,52 @@ d3.json(markers_url).then(function(response) {
 
       // Add a new marker to the cluster group and bind a pop-up
       markers.addLayer(L.marker(location)
-        .bindPopup(response.stations[i].name));
-    }
+        .bindPopup("<h5>" + response.stations[i].name + "</h5><hr>" + "Colonia: " + response.stations[i].districtName + "<br>Station ID: " + response.stations[i].id)
+        .on('mouseover', function (e) {
+          this.openPopup()
+        })
+        .on('mouseout', function (e) {
+          this.closePopup()
+        })
+        .addTo(markers) );
+    };
+  };
 
-  }
+
 
   // Add our marker cluster layer to the map
   myMap.addLayer(markers);
 
-  var markers_to_plot = new L.LayerGroup();
-  d3.json("static/temp_data/stationdata.json").then(markers => {
+// COLONIAS LAYER
+// ============================================================
+var colonias_layer = new L.LayerGroup();
+d3.json("static/temp_data/colonias.json").then(colonias => {
+  // console.log(colonias);
+  for (var i = 0; i < Object.keys(colonias).length; i++) {
+    var colonia_data = colonias[i]['geo_shape'].map(arr =>
+      [arr[1], arr[0]]
+    );
 
-  markers.stations.forEach(marker=> {
-    L.marker(marker.location)
-      .bindPopup("<h5>" + marker.name + "</h5><hr>" + "Colonia: "+ marker.districtName + "<br>Station ID: " + marker.id)
-      .on('mouseover', function (e) {
-        this.openPopup()
-      })
-      .on('mouseout', function (e) {
-        this.closePopup()
-      })
-      .addTo(markers_to_plot);
-      });
-  });
+    L.polygon(colonia_data, {
+      color: "#C73824",
+      fillColor: "transparent",
+      fillOpacity: 0,
+      weight: .5
+    }).addTo(colonias_layer);
+  };
+});
+// myMap.addLayer(colonias_layer);
 
-  // COLONIAS LAYER
-  // ============================================================
-  var colonias_layer = new L.LayerGroup();
-  d3.json("static/temp_data/colonias.json").then(colonias => {
-    console.log(colonias);
-    for (var i = 0; i < Object.keys(colonias).length; i++) {
-      var colonia_data = colonias[i]['geo_shape'].map(arr => 
-        [arr[1], arr[0]]
-        );
+var overlayMaps = {
+  'Station Markers': markers,
+  'Neighborhood Borders': colonias_layer,
 
-      L.polygon(colonia_data, {
-        color: "#C73824",
-        fillColor: "transparent",
-        fillOpacity: 0,
-        weight: .5
-      }).addTo(colonias_layer);
-    };
-  });
-  myMap.addLayer(colonias_layer);
+};
+
+
+L.control.layers(null, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
 
 });
 
